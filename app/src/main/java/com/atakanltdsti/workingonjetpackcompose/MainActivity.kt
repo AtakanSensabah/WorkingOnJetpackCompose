c05b088aabd72fbf06087631be8ba5d472823ffc
atakansensabah <atakansensabah@gmail.com>
2022-09-06 09:56:08 +0300

    Finished

--- app/src/main/java/com/atakanltdsti/workingonjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/atakanltdsti/workingonjetpackcompose/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.atakanltdsti.workingonjetpackcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,8 +20,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atakanltdsti.workingonjetpackcompose.ui.theme.WorkingOnJetpackComposeTheme
@@ -46,7 +49,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun content() {
+fun Content() {
     Box(
         modifier = Modifier
             .fillMaxHeight()
@@ -61,19 +64,44 @@ fun content() {
             shape = RoundedCornerShape(corner = CornerSize(6.dp)),
             border = BorderStroke(width = 2.dp, color = Color.LightGray)
         ) {
-            Portfolio(data = listOf("Project 1,","Project 2","Project 3"))
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
         }
     }
 }
 
 @Composable
 fun Portfolio(data: List<String>) {
-        LazyColumn{
-            items(data){
-                item ->  Text(item)
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(), shape = RectangleShape
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(16.dp)
+                ) {
+                    CreateProfileImage(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(
+                            text = "A kinda learning project",
+                            style = MaterialTheme.typography.body2
+                        )
+                    }
+                }
             }
         }
+    }
 }
+
 
 @Composable
 fun CreateBizCard() {
@@ -104,15 +132,14 @@ fun CreateBizCard() {
                 Divider(modifier = Modifier.padding(10.dp))
                 CreateInfo()
                 Button(onClick = {
-                    buttonClicked.value=!buttonClicked.value
+                    buttonClicked.value = !buttonClicked.value
                 }) {
                     Text(text = "Portfolio", style = MaterialTheme.typography.button)
                 }
-                if (buttonClicked.value){
-                    content()
-                }
-                else{
-                    Box(){}
+                if (buttonClicked.value) {
+                    Content()
+                } else {
+                    Box() {}
                 }
             }
 
@@ -143,9 +170,9 @@ private fun CreateInfo() {
 }
 
 @Composable
-private fun CreateProfileImage() {
+private fun CreateProfileImage(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -156,7 +183,7 @@ private fun CreateProfileImage() {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profileImage",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
